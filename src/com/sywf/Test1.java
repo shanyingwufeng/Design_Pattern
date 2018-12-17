4a9a83fd86e3c480ed77a172d8e4d33e03ef364f
xuyifan <xyfaqq@163.com>
2018-12-17 12:12:41 +0800

    删除Test1和Test2类

--- src/com/sywf/Test1.java
+++
@@ -1,9 +0,0 @@
-package com.sywf;
-
-public class Test1 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-    }
-}
--- src/com/sywf/Test2.java
+++
@@ -1,9 +0,0 @@
-package com.sywf;
-
-/**
- * Author: YiFan
- * Date: 2018/12/17 12:11
- * Description:
- */
-public class Test2 {
-}
